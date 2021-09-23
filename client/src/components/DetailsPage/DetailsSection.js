92c9a2c9c0faf52195ac7e288dbd51dc1cba47db
Phantom <phantom@pop-os.localdomain>
2021-09-23 17:06:08 +0530

    Feat: added states

--- client/src/components/DetailsPage/DetailsSection.js
+++ client/src/components/DetailsPage/DetailsSection.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const DetailsSection = () => {
+  const [state, setState] = useState({
+    uName: "",
+    about: "",
+    education: "",
+    education1: "",
+    work: "",
+    work1: "",
+    address: "",
+    number: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <form className="DetailsPage_Section_form" method="post">
-        <header className="DetailsPage_name">
-          <input
-            type="text"
-            id="DetailsPage_name"
-            placeholder="Your Name"
-          ></input>
-        </header>
+        <section className="DetailsPage_about">
+          Name:
+          <span className="DetailsPage_address">
+            <header className="DetailsPage_name">
+              <input
+                type="text"
+                id="DetailsPage_name"
+                placeholder="Your Name"
+                name="uName"
+                value={state.uName}
+                onchange={handleChange}
+              ></input>
+            </header>
+          </span>
+        </section>
         <section className="DetailsPage_about">
           About:
           <textarea
             id="DetailsPage_about"
             placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec at turpis fermentum, pharetra metus vitae, mattis quam. Quisque posuere laoreet neque, vel iaculis arcu lacinia in. Cras gravida eleifend ligula."
+            name="about"
+            value={state.about}
+            onChange={handleChange}
           ></textarea>
         </section>
         <section className="DetailsPage_education">
@@ -25,12 +55,18 @@ const DetailsSection = () => {
               <textarea
                 id="DetailsPage_education_1"
                 placeholder="Curabitur in lacus justo. Maecenas posuere luctus ultricies. Class aptent taciti sociosqu ad litora"
+                name="education"
+                value={state.education}
+                onChange={handleChange}
               />
             </li>
             <li>
               <textarea
                 id="DetailsPage_education_2"
                 placeholder="Curabitur in lacus justo. Maecenas posuere luctus ultricies. Class aptent taciti sociosqu ad litora"
+                name="education1"
+                value={state.education1}
+                onChange={handleChange}
               />
             </li>
           </ul>
@@ -42,12 +78,18 @@ const DetailsSection = () => {
               <textarea
                 id="DetailsPage_workExp_1"
                 placeholder="Curabitur in lacus justo. Maecenas posuere luctus ultricies. Class aptent taciti sociosqu ad litora"
+                name="work"
+                value={state.work}
+                onChange={handleChange}
               />
             </li>
             <li>
               <textarea
                 id="DetailsPage_workExp_2"
                 placeholder="Curabitur in lacus justo. Maecenas posuere luctus ultricies. Class aptent taciti sociosqu ad litora"
+                name="work1"
+                value={state.work1}
+                onChange={handleChange}
               />
             </li>
           </ul>
@@ -60,6 +102,9 @@ const DetailsSection = () => {
               type="text"
               id="DetailsPage_address"
               placeholder="XYZ, 123 , Pune, India"
+              name="address"
+              value={state.address}
+              onChange={handleChange}
             ></input>
           </span>
           <span className="DetailsPage_number">
@@ -68,6 +113,9 @@ const DetailsSection = () => {
               type="text"
               id="DetailsPage_number"
               placeholder="9823410569"
+              name="number"
+              value={state.number}
+              onChange={handleChange}
             ></input>
           </span>
           <span className="DetailsPage_email">
@@ -76,6 +124,9 @@ const DetailsSection = () => {
               type="text"
               id="DetailsPage_email"
               placeholder="abcdefghijk@gmail.com"
+              name="email"
+              value={state.email}
+              onChange={handleChange}
             ></input>
           </span>
         </section>
--- client/src/components/DetailsPage/ImageSection.js
+++ client/src/components/DetailsPage/ImageSection.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 const ImageSection = () => {
+  const [state, setState] = useState({
+    languages: "",
+    techSkills: "",
+    techSkills1: "",
+    hobbies: "",
+    hobbies1: "",
+  });
+
+  const handleChange = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
   return (
     <div>
       <img className="DetailsPage_displayPicture"></img>
       <form className="DetailsPage_Image_form" method="post">
         <section className="DetailsPage_languages">
           Languages:
-          <textarea id="DetailsPage_language" placeholder="English, French" />
+          <textarea
+            id="DetailsPage_language"
+            placeholder="English, French"
+            name="languages"
+            value={state.languages}
+            onChange={handleChange}
+          />
         </section>
         <section className="DetailsPage_techSkills">
           Technical Skills:
@@ -17,11 +38,17 @@ const ImageSection = () => {
                 type="text"
                 id="DetailsPage_techSkills_1"
                 placeholder="Lorem ipsum dolor sit amet"
+                name="techSkills"
+                value={state.techSkills}
+                onChange={handleChange}
               />
               <input
                 type="text"
                 id="DetailsPage_techSkills_2"
                 placeholder="Lorem ipsum dolor sit amet"
+                name="techSkills1"
+                value={state.techSkills1}
+                onChange={handleChange}
               />
             </li>
           </ul>
@@ -34,11 +61,17 @@ const ImageSection = () => {
                 type="text"
                 id="DetailsPage_hobbies_1"
                 placeholder="Sed nisl neque"
+                name="hobbies"
+                value={state.hobbies}
+                onChange={handleChange}
               />
               <input
                 type="text"
                 id="DetailsPage_hobbies_2"
                 placeholder="Sed nisl neque"
+                name="hobbies1"
+                value={state.hobbies1}
+                onChange={handleChange}
               />
             </li>
           </ul>
