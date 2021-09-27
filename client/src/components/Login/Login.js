e029a9373de6687221ef36e5492608dcdaa969b7
advaiswamy <advaiswamy@gmail.com>
2021-09-27 18:02:32 +0530

    Incomplete: UserAuth

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useSpring, animated } from "react-spring";
 import "../LoginPage/LoginPage.css";
+import axios from "axios";
 
 function Login() {
   const [username, setUsername] = useState("");
@@ -11,16 +12,55 @@ function Login() {
     from: { opacity: 0 },
   });
 
-  const handleSubmit = (e) => {};
+  const handlePwd = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleUname = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .post("http://localhost:5000/login", { username, password })
+      .then((res) => {
+        console.log(res);
+      });
+    // axios
+    //   .post(
+    //     "http://localhost:5000/login",
+    //     {},
+    //     {
+    //       auth: { username: { username }, password: { password } },
+    //     }
+    //   )
+    //   .then((res) => {
+    //     console.log(res);
+    //   });
+  };
+
   return (
     <animated.div style={animProp}>
       <h2>Login:</h2>
       <form onSubmit={handleSubmit}>
         <label for="u_name">Username:</label>
-        <input id="u_name" type="text" placeholder="username"></input>
+        <input
+          id="u_name"
+          type="text"
+          placeholder="username"
+          onChange={handleUname}
+          value={username}
+        ></input>
         <br />
         <label for="u_pwd">Password:</label>
-        <input id="u_pwd" type="password" placeholder="password"></input>
+        <input
+          id="u_pwd"
+          type="password"
+          placeholder="password"
+          onChange={handlePwd}
+          value={password}
+        ></input>
         <br />
         <br />
         <input type="submit" value="Login"></input>
